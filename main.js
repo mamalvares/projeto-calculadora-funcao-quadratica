b8535c5a8978305420592b32fd667999a022150f
Matheus Abrahão <matheus.alvares23@outlook.com>
2023-03-10 10:03:32 -0300

    update

--- main.js
+++ main.js
@@ -9,9 +9,11 @@ function calcularFuncaoQuadratica() {
         var bhaskaraX1 = (-digiteB + Math.sqrt(delta)) / 2 * digiteA;
         var bhaskaraX2 = (-digiteB - Math.sqrt(delta)) / 2 * digiteA;
 
-        document.getElementById("resultado").innerHTML = "A função é redutível (delta =" + delta.toFixed(2) + ")";
-        document.getElementById("reduzida").innerHTML = "E a equação reduzida é " + digiteA + "(X" + bhaskaraX1.toFixed(2) + ")(X" + bhaskaraX2.toFixed(2) + ")";
+        document.getElementById("resultado").innerHTML = "A função é redutível (delta = √" + delta.toFixed(2) + ")";
+        document.getElementById("bhaskarax1").innerHTML = "X1 = " + bhaskaraX1.toFixed(2);
+        document.getElementById("bhaskarax2").innerHTML = "X2 = " + bhaskaraX2.toFixed(2);
+        document.getElementById("reduzida").innerHTML = "A equação reduzida é: <br> " + digiteA + "(X - " + bhaskaraX1.toFixed(2) + ")(X - " + bhaskaraX2.toFixed(2) + ")";
     } else {
-        document.getElementById("resultado").innerHTML = "A função é irredutível (delta =" + delta.toFixed(2) + ")";
+        document.getElementById("resultado").innerHTML = "A função é irredutível (delta = " + delta.toFixed(2) + ")";
     }
 }
